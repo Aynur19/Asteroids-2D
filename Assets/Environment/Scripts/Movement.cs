461032f1dc4a62d9d395cfd209fb32a574aed8b0
Aynur19 <nasybullin.aynur@yandex.ru>
2021-07-05 21:32:14 +0300

    add player rotation 2d

--- Assets/Environment/Scripts/Movement.cs
+++ Assets/Environment/Scripts/Movement.cs
@@ -5,8 +5,27 @@ public class Movement : MonoBehaviour
 	[SerializeField]
 	private float rotationSpeed = 10f;
 
+	private PlayerInput actions;
+
+	private void Awake()
+	{
+		actions = new PlayerInput();
+	}
+
+	private void OnEnable()
+	{
+		actions.Enable();
+	}
+
+	private void OnDisable()
+	{
+		actions.Disable();
+	}
+
 	private void FixedUpdate()
 	{
-		transform.Rotate(new Vector3(0, 0, rotationSpeed));
+		var move = actions.Player.Move.ReadValue<Vector2>();
+
+		transform.Rotate(new Vector3(0, 0, -move.x * rotationSpeed));
 	}
 }
