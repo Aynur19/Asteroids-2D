5cd68cfc995b594b80611c44838965a8e00af022
Aynur19 <nasybullin.aynur@yandex.ru>
2021-07-10 16:29:02 +0300

    refactoring

--- Assets/Environment/Scripts/Movement.cs
+++ Assets/Environment/Scripts/Movement.cs
@@ -1,15 +1,26 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
 	[SerializeField]
-	private float rotationSpeed = 10f;
+	private float rotationSpeed = 2f;
+
+	[SerializeField]
+	private float moveSpeed = 2f;
 
 	private PlayerInput actions;
+	private Rigidbody2D rb2D;
 
 	private void Awake()
 	{
 		actions = new PlayerInput();
+		actions.Player.Fire.performed += x => Check();
+	}
+
+	private void Check()
+	{
+		rb2D.velocity = new Vector2(0f, 0f);
 	}
 
 	private void OnEnable()
@@ -22,10 +33,19 @@ public class Movement : MonoBehaviour
 		actions.Disable();
 	}
 
+	private void Start()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
 	private void FixedUpdate()
 	{
 		var move = actions.Player.Move.ReadValue<Vector2>();
 
-		transform.Rotate(new Vector3(0, 0, -move.x * rotationSpeed));
+		if (move.y > 0f)
+		{
+			rb2D.AddForce(transform.up * 5);
+		}
+		rb2D.AddTorque(-move.x * rotationSpeed);
 	}
 }
